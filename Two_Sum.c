f0bbef5883e42e930d2210cba095e25a50320a91
Maxim Durnea <70955732+BarSick69@users.noreply.github.com>
2023-11-29 00:41:42 +0200

    Update Two_Sum.c

--- Two_Sum.c
+++ Two_Sum.c
@@ -1,4 +1,9 @@
 /**
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
  * Note: The returned array must be malloced, assume caller calls free().
  */
  #include <stdio.h>
@@ -6,7 +11,7 @@
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
         int *returnPair = (int *)malloc(sizeof(int)*2); 
         for(int i = 0; i<numsSize-1 ; i++){
-            for(int j = 1;j<numsSize;j++){
+            for(int j = i+1;j<numsSize;j++){
                 if(nums[i]+nums[j]==target){
                     returnPair[0]=i;
                     returnPair[1]=j;
