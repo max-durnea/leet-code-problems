a4b5599109d133f16da3a717fb4502d9960f6759
Maxim Durnea <70955732+BarSick69@users.noreply.github.com>
2023-11-29 01:02:26 +0200

    Create Build-array-from-permutations.c

---
+++ Build-array-from-permutations.c
@@ -0,0 +1,16 @@
+/**
+Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+ 
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* buildArray(int* nums, int numsSize, int* returnSize) {
+    int *ans=(int *)malloc(numsSize*sizeof(int));
+    *returnSize=numsSize;
+    for(int i = 0;i < numsSize; i++){
+        ans[i]=nums[nums[i]];
+    }
+    return ans;
+}
